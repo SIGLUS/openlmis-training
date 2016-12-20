a4a6f8022fcf611248d05b3616512bc7b820accb
Hanxian Lin <saitoplam@hotmail.com>
2016-12-21 00:23:16 +0800

    add set data

--- setup_db.sh
+++ setup_db.sh
@@ -4,6 +4,15 @@
 DB_ID=$(docker ps -aqf "name=trainingdb")
 
 echo $DB_ID
-ehco 'copy dump to db'
+echo "copy dump to db"
+
 # copy data to trainingdb container, and set data
-docker cp dumpForTraining.sql $DB_ID:/dumpForTraining.sql && docker exec -t $DB_ID psql -d open_lmis -U openlmis -a -f ./dumpForTraining.sql
+docker cp dumpForTraining.sql $DB_ID:/dumpForTraining.sql
+
+echo "dump has benn copied"
+
+echo "========================="
+echo $DB_ID
+echo "========================="
+
+docker exec -t $DB_ID [ -f ./dumpForTraining.sql ] && psql -d open_lmis -U openlmis -a -f ./dumpForTraining.sql
