8da250598755c71ff3e09f2c51810dfad04425f2
Danni Yu <dyu@thoughtworks.com>
2017-08-09 15:52:20 -0500

    DE00 DY - add one time migration nos default false to training env

--- build-openlmis-localised/run_time/migration.sh
+++ build-openlmis-localised/run_time/migration.sh
@@ -10,7 +10,7 @@ do
 done
 
 
-echo "import data"
+echo "Import data"
 PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB < /run_time/dumpForTraining.sql && echo "Data import done"
 
 echo "Change timestamp"
@@ -37,6 +37,9 @@ echo "Run migrations"
  -baselineOnMigrate=true \
  -locations=filesystem:/opt/flyway/sql/migration \
  migrate
+
+echo "Run one time migrations"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB < /run_time/oneTimeMigrations.sql && echo "One time migrations done"
 
 echo "Generate reports"
 PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB < /run_time/generateReportViews.sql && echo "Report generation done, please open localhost:8080 in browser"
---
+++ build-openlmis-localised/run_time/oneTimeMigrations.sql
@@ -0,0 +1,1 @@
+UPDATE products SET nos = FALSE;
